ef84e4e39a840112f9eb8b427a46a3675afc7851
danachen <danac.tw@gmail.com>
2021-12-21 12:12:25 +0800

    feat:edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :backup_email, :backup_phone])
+  end
 end
